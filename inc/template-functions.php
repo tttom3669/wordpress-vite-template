371b8a864da4ff0db5257602448f1581afadeaee
tttom3669 <tttom3669@gmail.com>
2024-12-25 11:47:59 +0800

    update:php slug

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -2,7 +2,7 @@
 /**
  * Functions which enhance the theme by hooking into WordPress
  *
- * @package test
+ * @package vite-template
  */
 
 /**
@@ -11,7 +11,7 @@
  * @param array $classes Classes for the body element.
  * @return array
  */
-function test_body_classes( $classes ) {
+function vite_template_body_classes( $classes ) {
 	// Adds a class of hfeed to non-singular pages.
 	if ( ! is_singular() ) {
 		$classes[] = 'hfeed';
@@ -24,14 +24,14 @@ function test_body_classes( $classes ) {
 
 	return $classes;
 }
-add_filter( 'body_class', 'test_body_classes' );
+add_filter( 'body_class', 'vite_template_body_classes' );
 
 /**
  * Add a pingback url auto-discovery header for single posts, pages, or attachments.
  */
-function test_pingback_header() {
+function vite_template_pingback_header() {
 	if ( is_singular() && pings_open() ) {
 		printf( '<link rel="pingback" href="%s">', esc_url( get_bloginfo( 'pingback_url' ) ) );
 	}
 }
-add_action( 'wp_head', 'test_pingback_header' );
+add_action( 'wp_head', 'vite_template_pingback_header' );
