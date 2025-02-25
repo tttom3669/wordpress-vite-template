072351ebb9f59a2bdcbadf0f0a914e70ed281523
tttom3669 <tttom3669@gmail.com>
2025-02-25 14:10:37 +0800

    update:fix package name

--- sidebar.php
+++ sidebar.php
@@ -4,7 +4,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package test
+ * @package vite-template
  */
 
 if ( ! is_active_sidebar( 'sidebar-1' ) ) {
